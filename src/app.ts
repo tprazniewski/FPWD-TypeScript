47ef65d04ee20a52efc871ba1f1de7eb0a3c9aad
tprazniewski <104758997+tprazniewski@users.noreply.github.com>
2022-10-31 15:12:14 +0100

    Create DB connection using type

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,38 @@
 // const express = require('express')
-import express, {Express,Request, Response, NextFunction} from 'express'
-import questionRoutes from './routes/questions'
-import dotenv from 'dotenv'
-
-dotenv.config()
+import express, {
+  Express,
+  Request,
+  Response,
+  NextFunction,
+} from 'express';
+import questionRoutes from './routes/questions';
+import { appDataSource } from './DB/mysql';
 
 const app = express();
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
 app.get('/', (_, res) => {
-    res.status(200).json({ message: 'Welcome to FPWD!' })
-})
+  res.status(200).json({ message: 'Welcome to FPWD!' });
+});
 
-app.use('/questions', questionRoutes)
+app.use('/questions', questionRoutes);
 
-app.use((err: Error, req:Request, res: Response, next: NextFunction) => {
-    res.status(500).json({message: err.message})
-})
-
-
-app.listen(PORT)
+app.use(
+  (
+    err: Error,
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) => {
+    res.status(500).json({ message: err.message });
+  },
+);
+appDataSource
+  .initialize()
+  .then(() => {
+    app.listen(PORT);
+    console.log('Data has been initilized!');
+  })
+  .catch((err) => {
+    console.error('error During Db init', err);
+  });
--- src/controllers/questions.ts
+++ src/controllers/questions.ts
@@ -1,26 +1,17 @@
-import express, {RequestHandler} from 'express'
+import express, { RequestHandler } from 'express';
 
+const getQuestions: RequestHandler = (req, res, next) => {};
 
-const getQuestions:RequestHandler = ((req, res, next)=>{
-    
-})
+const getQuestionById: RequestHandler = (
+  req,
+  res,
+  next,
+) => {};
 
-const getQuestionById:RequestHandler = ((req, res, next)=>{
-    
-})
+const addQuestion: RequestHandler = (req, res, next) => {};
 
-const addQuestion:RequestHandler = ((req, res, next) =>{
-    
-})
+const getAnswers: RequestHandler = (req, res, next) => {};
 
-const getAnswers:RequestHandler = ((req, res, next) => {
+const addAnswer: RequestHandler = (req, res, next) => {};
 
-})
-
-const addAnswer:RequestHandler = ((req, res, next) => {
-    
-})
-
-const getAnswer:RequestHandler = ((req, res, next) =>{
-
-})
+const getAnswer: RequestHandler = (req, res, next) => {};
