e5a1b4090cc62ab31cf008ca2c8c2c81df675ce7
tprazniewski <104758997+tprazniewski@users.noreply.github.com>
2022-11-01 22:25:46 +0100

    Update answers.ts

--- src/routes/answers/answers.ts
+++ src/routes/answers/answers.ts
@@ -1,7 +1,22 @@
 import { Router, Request, Response } from 'express';
 import { Tquestion } from '../../entities/question';
 import { Tanswer } from '../../entities/answer';
+import { appDataSource } from '../../DB/mysql';
+
 const router = Router({ mergeParams: true }); //This allow us to register middleware
+
+router.get(
+  '/',
+  async (req: Request<{ questionId: string }>, res) => {
+    // const useRepo = appDataSource.getRepository(Tanswer);
+    // const answ = useRepo.find({ tquestion_id: 1 });
+    const { questionId } = req.params;
+    const tquestion = await Tquestion.find({
+      relations: ['tanswer'],
+    });
+    const answers = Tanswer.findOneBy({});
+  },
+);
 
 router.post(
   '/',
