9723ebfd033a3742c00a00de30f52ae5e38e3f5c
tprazniewski <104758997+tprazniewski@users.noreply.github.com>
2022-11-08 22:24:31 +0100

    update

--- src/entities/question.ts
+++ src/entities/question.ts
@@ -1,5 +1,5 @@
 //is never instantiated by tbe application is only used by typed ORM
-import { Entity, ManyToOne, OneToMany } from 'typeorm';
+import { Entity, OneToMany } from 'typeorm';
 // import { QuestionTS } from './questions';
 import { Base } from './utils/baseForAnswersAndQuestions';
 import { Tanswer } from './answer';
